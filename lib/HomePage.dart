74659449657b13a427c60efc68742bdba57fc661
Batuhan <batuhan.kizil@outlook.com>
2022-03-13 00:41:02 +0300

    text

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
@@ -25,69 +26,96 @@ class HomePage extends StatelessWidget {
           ),
           centerTitle: true,
         ),
-        body: Container(
-          height: 180,
-          child: ListView(
-            scrollDirection: Axis.horizontal,
-            children: <Widget>[
-              Container(
-                margin: EdgeInsets.all(10),
-                width: 200,
-                decoration: BoxDecoration(
-                    //color: Colors.red.shade400,
-                  gradient: LinearGradient(
-                    colors: [Colors.green.shade400, Colors.green.shade700],
-                    begin: Alignment.bottomLeft,
-                    end: Alignment.topRight,
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+             Container(
+               margin: const EdgeInsets.only(top: 10, left: 8),
+              child: const Text("Dersler",
+              style: TextStyle(/*fontWeight: FontWeight.bold,*/ fontSize: 26, fontWeight: FontWeight.w300),
+              textAlign: TextAlign.left,),
+            ),
+            Container(
+              height: 180,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.all(10),
+                    width: 200,
+                    decoration: BoxDecoration(
+                        //color: Colors.red.shade400,
+                        gradient: LinearGradient(
+                          colors: [Colors.green.shade400, Colors.green.shade700],
+                          begin: Alignment.bottomLeft,
+                          end: Alignment.topRight,
+                        ),
+                        borderRadius: BorderRadius.circular(20)),
+                    child: const Center(
+                        child: Text(
+                      'Matematik',
+                      style: TextStyle(fontSize: 18, color: Colors.white),
+                    )),
                   ),
-                    borderRadius: BorderRadius.circular(20)
-                ),
-                child: const Center(child: Text('Matematik', style: TextStyle(fontSize: 18, color: Colors.white),)),
+                  Container(
+                    margin: EdgeInsets.all(10),
+                    width: 200,
+                    decoration: BoxDecoration(
+                        //color: Colors.red.shade400,
+                        gradient: LinearGradient(
+                          colors: [Colors.green.shade400, Colors.green.shade700],
+                          begin: Alignment.bottomLeft,
+                          end: Alignment.topRight,
+                        ),
+                        borderRadius: BorderRadius.circular(20)),
+                    child: const Center(
+                        child: Text(
+                      'Türkçe',
+                      style: TextStyle(fontSize: 18, color: Colors.white),
+                    )),
+                  ),
+                  Container(
+                    margin: EdgeInsets.all(10),
+                    width: 200,
+                    decoration: BoxDecoration(
+                        //color: Colors.red.shade400,
+                        gradient: LinearGradient(
+                          colors: [Colors.green.shade400, Colors.green.shade700],
+                          begin: Alignment.bottomLeft,
+                          end: Alignment.topRight,
+                        ),
+                        borderRadius: BorderRadius.circular(20)),
+                    child: const Center(
+                        child: Text(
+                      'Fizik',
+                      style: TextStyle(fontSize: 18, color: Colors.white),
+                    )),
+                  ),
+                  Container(
+                    margin: EdgeInsets.all(10),
+                    width: 200,
+                    decoration: BoxDecoration(
+                        //color: Colors.red.shade400,
+                        gradient: LinearGradient(
+                          colors: [Colors.green.shade400, Colors.green.shade700],
+                          begin: Alignment.bottomLeft,
+                          end: Alignment.topRight,
+                        ),
+                        borderRadius: BorderRadius.circular(20)),
+                    child: const Center(
+                        child: Text(
+                      'Ingilizce',
+                      style: TextStyle(fontSize: 18, color: Colors.white),
+                    )),
+                  ),
+                ],
               ),
-              Container(
-                margin: EdgeInsets.all(10),
-                width: 200,
-                decoration: BoxDecoration(
-                    //color: Colors.red.shade400,
-                    gradient: LinearGradient(
-                      colors: [Colors.green.shade400, Colors.green.shade700],
-                      begin: Alignment.bottomLeft,
-                      end: Alignment.topRight,
-                    ),
-                    borderRadius: BorderRadius.circular(20)
-                ),
-                child: const Center(child: Text('Türkçe', style: TextStyle(fontSize: 18, color: Colors.white),)),
-              ),
-              Container(
-                margin: EdgeInsets.all(10),
-                width: 200,
-                decoration: BoxDecoration(
-                    //color: Colors.red.shade400,
-                    gradient: LinearGradient(
-                      colors: [Colors.green.shade400, Colors.green.shade700],
-                      begin: Alignment.bottomLeft,
-                      end: Alignment.topRight,
-                    ),
-                    borderRadius: BorderRadius.circular(20)
-                ),
-                child: const Center(child: Text('Fizik', style: TextStyle(fontSize: 18, color: Colors.white),)),
-              ),
-              Container(
-                margin: EdgeInsets.all(10),
-                width: 200,
-                decoration: BoxDecoration(
-                    //color: Colors.red.shade400,
-                    gradient: LinearGradient(
-                      colors: [Colors.green.shade400, Colors.green.shade700],
-                      begin: Alignment.bottomLeft,
-                      end: Alignment.topRight,
-                    ),
-                    borderRadius: BorderRadius.circular(20)
-                ),
-                child: const Center(child: Text('Ingilizce', style: TextStyle(fontSize: 18, color: Colors.white),)),
-              ),
-            ],
-          ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 5, left: 12),
+              child: Text('Yapılıyor...'),
+            )
+          ],
         ),
       ),
     );
