cae0b1413458e86ad53646dec70a55f2cec7d0f5
Batuhan <batuhan.kizil@outlook.com>
2022-06-15 13:21:12 +0300

    son

--- lib/favNotes.dart
+++ lib/favNotes.dart
@@ -41,7 +41,7 @@ class _favNotesScreenState extends State<favNotesScreen> {
         child: Column(
           children: [
             const SizedBox(
-              height: 80,
+              height: 60,
             ),
             Expanded(
               flex: 3,
@@ -60,9 +60,186 @@ class _favNotesScreenState extends State<favNotesScreen> {
                               const SizedBox(
                                 height: 20,
                               ),
-                              Text('Pazartesi',style: TextStyle(color: HexColor("32a852"),fontSize: 20,fontWeight: FontWeight.bold)),
-                              Text('Test',style: TextStyle(color: HexColor("32a852"),fontSize: 20,fontWeight: FontWeight.bold)),
-
+                              Text('Sayısal'),
+                              Padding(
+                                padding: const EdgeInsets.all(12),
+                                child: Table(
+                                    border: TableBorder.all(), // Allows to add a border decoration around your table
+                                    children: [
+                                      TableRow(children :[
+                                        Text('Pazartesi'),
+                                        Text('Salı'),
+                                        Text('Çarşamba'),
+                                        Text('Perşembe'),
+                                      ]),
+                                      TableRow(children :[
+                                        Text('2011',),
+                                        Text('Dart'),
+                                        Text('Lars Bak'),
+                                        Text('Lars Bak'),
+                                      ]),
+                                      TableRow(children :[
+                                        Text('1996'),
+                                        Text('Java'),
+                                        Text('James Gosling'),
+                                        Text('James Gosling'),
+                                      ]),
+                                      TableRow(children :[
+                                        Text('1996'),
+                                        Text('Java'),
+                                        Text('James Gosling'),
+                                        Text('James Gosling'),
+                                      ]),
+                                      TableRow(children :[
+                                        Text('1996'),
+                                        Text('Java'),
+                                        Text('James Gosling'),
+                                        Text('James Gosling'),
+                                      ]),
+                                      TableRow(children :[
+                                        Text('1996'),
+                                        Text('Java'),
+                                        Text('James Gosling'),
+                                        Text('James Gosling'),
+                                      ]),
+                                      TableRow(children :[
+                                        Text('1996'),
+                                        Text('Java'),
+                                        Text('James Gosling'),
+                                        Text('James Gosling'),
+                                      ]),
+                                      TableRow(children :[
+                                        Text('1996'),
+                                        Text('Java'),
+                                        Text('James Gosling'),
+                                        Text('James Gosling'),
+                                      ]),
+                                    ]
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(12),
+                                child: Table(
+                                    border: TableBorder.all(), // Allows to add a border decoration around your table
+                                    children: [
+                                      TableRow(children :[
+                                        Text('Cuma'),
+                                        Text('Cumartesi'),
+                                        Text('Pazar'),
+                                      ]),
+                                      TableRow(children :[
+                                        Text('2011',),
+                                        Text('Dart'),
+                                        Text('Lars Bak'),
+                                      ]),
+                                      TableRow(children :[
+                                        Text('1996'),
+                                        Text('Java'),
+                                        Text('James Gosling'),
+                                      ]),
+                                    ]
+                                ),
+                              ),
+                              Text('Sözel'),
+                              Padding(
+                                padding: const EdgeInsets.all(12),
+                                child: Table(
+                                    border: TableBorder.all(), // Allows to add a border decoration around your table
+                                    children: [
+                                      TableRow(children :[
+                                        Text('Pazartesi'),
+                                        Text('Salı'),
+                                        Text('Çarşamba'),
+                                        Text('Perşembe'),
+                                      ]),
+                                      TableRow(children :[
+                                        Text('2011',),
+                                        Text('Dart'),
+                                        Text('Lars Bak'),
+                                        Text('Lars Bak'),
+                                      ]),
+                                      TableRow(children :[
+                                        Text('1996'),
+                                        Text('Java'),
+                                        Text('James Gosling'),
+                                        Text('James Gosling'),
+                                      ]),
+                                    ]
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(12),
+                                child: Table(
+                                    border: TableBorder.all(), // Allows to add a border decoration around your table
+                                    children: [
+                                      TableRow(children :[
+                                        Text('Cuma'),
+                                        Text('Cumartesi'),
+                                        Text('Pazar'),
+                                      ]),
+                                      TableRow(children :[
+                                        Text('2011',),
+                                        Text('Dart'),
+                                        Text('Lars Bak'),
+                                      ]),
+                                      TableRow(children :[
+                                        Text('1996'),
+                                        Text('Java'),
+                                        Text('James Gosling'),
+                                      ]),
+                                    ]
+                                ),
+                              ),
+                              Text('Eşit Ağırlık'),
+                              Padding(
+                                padding: const EdgeInsets.all(12),
+                                child: Table(
+                                    border: TableBorder.all(), // Allows to add a border decoration around your table
+                                    children: [
+                                      TableRow(children :[
+                                        Text('Pazartesi'),
+                                        Text('Salı'),
+                                        Text('Çarşamba'),
+                                        Text('Perşembe'),
+                                      ]),
+                                      TableRow(children :[
+                                        Text('2011',),
+                                        Text('Dart'),
+                                        Text('Lars Bak'),
+                                        Text('Lars Bak'),
+                                      ]),
+                                      TableRow(children :[
+                                        Text('1996'),
+                                        Text('Java'),
+                                        Text('James Gosling'),
+                                        Text('James Gosling'),
+                                      ]),
+                                    ]
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(12),
+                                child: Table(
+                                    border: TableBorder.all(), // Allows to add a border decoration around your table
+                                    children: [
+                                      TableRow(children :[
+                                        Text('Cuma'),
+                                        Text('Cumartesi'),
+                                        Text('Pazar'),
+                                      ]),
+                                      TableRow(children :[
+                                        Text('2011',),
+                                        Text('Dart'),
+                                        Text('Lars Bak'),
+                                      ]),
+                                      TableRow(children :[
+                                        Text('1996'),
+                                        Text('Java'),
+                                        Text('James Gosling'),
+                                      ]),
+                                    ]
+                                ),
+                              ),
                             ],
                     )
                 ),
